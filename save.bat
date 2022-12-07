2eccebc28bb1ec580bb7a71fa0203aac325b2ce4
Hehe <Hehe@gmail.com>
2022-12-07 13:24:32 -0300

    search button

--- save.bat
+++ save.bat
@@ -6,7 +6,7 @@ set /p commit=Enter Commit:
 git commit -m "%commit%"
 git config --global user.name Felifelps
 git config --global user.email felipefelipe23456@gmail.com
-git push https://github.com/Felifelps/CifraEdit version1.0
+git push https://github.com/Felifelps/CifraNote version1.0
 git config --global user.name Hehe
 git config --global user.email Hehe@gmail.com
 echo Salvo
