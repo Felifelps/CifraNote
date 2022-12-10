7861240386186359a634888643eb32d017901a33
Hehe <Hehe@gmail.com>
2022-12-10 14:05:58 -0300

    trying to add style

--- mainpage.py
+++ mainpage.py
@@ -22,27 +22,8 @@ class MainPage(Screen):
         return super().on_pre_enter(*args)
 
 class SearchBar(RelativeLayout):
-    def __init__(self, **kwargs):
-        super(SearchBar, self).__init__(**kwargs)
-        self.textinput = TextInput(
-            multiline=False,
-            text="",
-            size_hint=(.7, .7),
-            pos_hint={"center_x": .45, "center_y": .5},
-            font_size='27sp',
-            hint_text="Pesquisar cifra"
-        )
-        self.textinput.bind(on_text_validate=self.search)
-        self.add_widget(self.textinput)
-        
-        self.searchbutton = SearchButton(
-            size_hint=(.125, .725),
-            pos_hint={"right": .9, "center_y": .5},
-            on_press=self.search 
-        )
-        self.add_widget(self.searchbutton)
-
-    def search(self, instance):
+    def search(self):
+        print("hi")
         self.root.filebuttons.remake_organization(self.textinput.text)
 
 class CreateFileButton(Button):
@@ -54,9 +35,6 @@ class CreateFileButton(Button):
         manager = self.root.root.manager
         manager.current = 'editpage'
         manager.ids['editpage'].model.new_file()
-
-class SearchButton(Button):
-    pass
 
 class CreditsButton(Button):
     def __init__(self, **kwargs):
