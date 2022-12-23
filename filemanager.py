b6a9fc295cae5274c178d3d5ea91947c0291d9f3
Felifelps <felipefelipe23456@gmail.com>
2022-12-23 15:09:38 -0300

    first version

--- filemanager.py
+++ filemanager.py
@@ -34,35 +34,30 @@ class FileManager:
     
     def delete(self, title):
         os.remove(os.path.join(self.main_path, f"{title}"))
-        self.update()        
-
-from times import times
-if times == 0:
-    with open(os.path.abspath("cifras\Tutorial"), "w") as arq:
-        arq.write('''__TONE__:C
-    Seja bem-vindo ao CifraNote!!
-
-    Desenvolvi esse aplicativo para servir como bloco de notas para cifras, com a funcionalidade especial de trocar o tom das cifras.
-
-    Para criar uma nova cifra, clique no botão de "+" na página inicial. 
-
-    Para excluir um arquivo, basta apertar e segurar no botão do seu nome até aparecer a função "Excluir". Clique e confirme.
-
-    A caixa de texto com o nome "Tutorial" acima serve para alterar o nome do arquivo. 
-
-    Para trocar o tom de uma cifra, use os botões "-Semitom" e "+Semitom" para diminuir ou aumentar, respectivamente, o tom da cifra atual.
-
-    Lembre sempre de salvar a cifra antes de sair.
-
-    Tente trocar o tom: 
-    C G Am F
-
-    Aproveite o aplicativo!!
-    ''')
-    times += 1
-if times > 0:
-    with open("times.py", "w") as arq:
-        arq.write(f"times = {times + 1}")
-        
+        self.update()     
+           
 FILEMANAGER = FileManager()
+if "__Tutorial__" in FILEMANAGER.files:
+    FILEMANAGER.save("__Tutorial__", "C", '''
+Seja bem-vindo ao CifraNote!!
+
+Desenvolvi esse aplicativo para servir como bloco de notas para cifras, com a funcionalidade especial de trocar o tom das cifras.
+
+Para criar uma nova cifra, clique no botão de "+" na página inicial. 
+
+Para excluir um arquivo, basta apertar e segurar no botão do seu nome até aparecer a função "Excluir". Clique e confirme.
+
+A caixa de texto com o nome "Tutorial" acima serve para alterar o nome do arquivo. 
+
+Para trocar o tom de uma cifra, use os botões "-Semitom" e "+Semitom" para diminuir ou aumentar, respectivamente, o tom da cifra atual.
+
+Lembre sempre de salvar a cifra antes de sair.
+
+Tente trocar o tom: 
+C G Am F
+
+Aproveite o aplicativo!!
+    ''', False)
+        
+
 
